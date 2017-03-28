e7dda810ec257655adbb986d7bf15e1a63b2ff68
njsoly <njsoly@polymuler.com>
2017-03-28 03:24:01 -0500

    new header for serial I/O.

--- Mixolotron.h
+++ Mixolotron.h
@@ -5,6 +5,7 @@
 #include "Mixolotron_EEPROM.h"
 #include "Mixolotron_TFT.h"
 #include "Mixolotron_keypad.h"
+#include "Mixolotron_serial.h"
 
 
 #endif
---
+++ Mixolotron_serial.h
@@ -0,0 +1,8 @@
+/**** Mixolotron_serial.h ****/
+#ifndef MIXOLOTRON_SERIAL_H_
+#define MIXOLOTRON_SERIAL_H_
+
+
+
+
+#endif // MIXOLOTRON_SERIAL_H_
